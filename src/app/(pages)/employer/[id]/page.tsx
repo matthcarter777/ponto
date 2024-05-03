ac2116eeb3001f5e342cf73c76fed8076e685d52
Mateus Henrique <mateushbsb@gmail.com>
2024-05-03 20:40:21 -0300

    relogio

--- src/app/(pages)/employer/[id]/page.tsx
+++ src/app/(pages)/employer/[id]/page.tsx
@@ -1,7 +1,26 @@
+'use client'
 import { Header } from "@/app/components/Header";
-import { Flex, SimpleGrid, Button  } from "@chakra-ui/react";
+import { 
+  Flex, 
+  SimpleGrid, 
+  Button,
+  Text,
+} from "@chakra-ui/react";
+import React, { useState, useEffect } from 'react';
 
 export default function Employer() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  const currentDate = new Date();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
 
   return (
     <>
@@ -11,16 +30,20 @@ export default function Employer() {
         h="100%"
         my="6"
         maxWidth={1480}
-        padding="10"  
+        padding="10"
+        direction="column"
+        alignItems="center"  
       >
-        <SimpleGrid columns={4} spacing={5}>
-          <Button >Entrada</Button>
-          <Button>Intervalo</Button>
-          <Button>S. Intervalo</Button>
-          <Button>Saida</Button>
+        <Text mb="3rem">{currentDate.toLocaleDateString()},{currentTime.toLocaleTimeString()}</Text>
+        <SimpleGrid columns={4} spacing={10}>
+          <Button w='5rem' mr='3rem' fontSize="11px" >Entrada</Button>
+          <Button w='5rem' mr='3rem' fontSize="11px" >Intervalo</Button>
+          <Button w='5rem' mr='3rem' fontSize="11px" >S. Intervalo</Button>
+          <Button w='5rem' mr='3rem' fontSize="11px" >Saida</Button>
         </SimpleGrid>
         
       </Flex>
+
     </>
-  )
+  );
 }
--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,11 +1,14 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import { SidebarDrawerProvider } from './hooks/SidebarDrawerContext';
 
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      <SidebarDrawerProvider>
-        {children}
-      </SidebarDrawerProvider>
+      <ChakraProvider>
+        <SidebarDrawerProvider>
+          {children}
+        </SidebarDrawerProvider>
+      </ChakraProvider>
     </div>
   )
 }
